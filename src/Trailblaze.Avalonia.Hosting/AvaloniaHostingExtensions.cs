a3d98b65939da3f25440b1105858dd0284e698ae
Aruden <84431560+ArudenKun@users.noreply.github.com>
2025-06-15 21:26:24 +0800

    added translations and hosting

---
+++ src/Trailblaze.Avalonia.Hosting/AvaloniaHostingExtensions.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Trailblaze.Avalonia.Hosting.Internals;
+
+namespace Trailblaze.Avalonia.Hosting;
+
+/// <summary>
+/// Provides extension methods for configuring Avalonia applications with the generic host.
+/// </summary>
+public static class AvaloniaHostingExtensions
+{
+    internal const string ServiceProviderResourceKey =
+        $"ServiceProvider.{nameof(AvaloniaHostingExtensions)}.{nameof(AvaloniaHostedService)}";
+
+    public static IServiceProvider GetServiceProvider(this Application? app) =>
+        app?.Resources[ServiceProviderResourceKey] as IServiceProvider
+        ?? throw new InvalidOperationException("Avalonia not yet initialized");
+
+    /// <summary>
+    /// Adds Avalonia main window to the host's service collection,
+    /// and a <see cref="AppBuilder"/> to create the Avalonia application.
+    /// </summary>
+    /// <param name="services">The IServiceCollection.</param>
+    /// <param name="appBuilderConfiguration">The application builder</param>
+    /// <returns>The updated host application builder.</returns>
+    public static IServiceCollection AddAvaloniaHosting<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TApp
+    >(
+        this IServiceCollection services,
+        Action<IServiceProvider, AppBuilder> appBuilderConfiguration
+    )
+        where TApp : Application
+    {
+        services
+            .AddSingleton<TApp>()
+            .AddSingleton<Application>(sp => sp.GetRequiredService<TApp>())
+            .AddSingleton(sp =>
+            {
+                var appBuilder = AppBuilder.Configure(sp.GetRequiredService<TApp>);
+                appBuilderConfiguration(sp, appBuilder);
+                return appBuilder;
+            })
+            .AddSingleton<IClassicDesktopStyleApplicationLifetime>(_ =>
+                (IClassicDesktopStyleApplicationLifetime?)Application.Current?.ApplicationLifetime
+                ?? throw new InvalidOperationException("Avalonia application lifetime is not set.")
+            )
+            .AddSingleton<AvaloniaThread>()
+            .AddHostedService<AvaloniaHostedService>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds Avalonia main window to the host's service collection,
+    /// and a <see cref="AppBuilder"/> to create the Avalonia application.
+    /// </summary>
+    /// <param name="builder">The host application builder.</param>
+    /// <param name="appBuilder">The application builder, also used by the previewer.</param>
+    /// <returns>The updated host application builder.</returns>
+    public static IHostApplicationBuilder AddAvaloniaHosting<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TApp
+    >(this IHostApplicationBuilder builder, Action<IServiceProvider, AppBuilder> appBuilder)
+        where TApp : Application
+    {
+        builder.Services.AddAvaloniaHosting<TApp>(appBuilder);
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds Avalonia main window to the host's service collection,
+    /// and a <see cref="AppBuilder"/> to create the Avalonia application.
+    /// </summary>
+    /// <param name="builder">The host application builder.</param>
+    /// <param name="appBuilderAction">The application builder, also used by the previewer.</param>
+    /// <returns>The updated host application builder.</returns>
+    public static IHostApplicationBuilder AddAvaloniaHosting<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TApp
+    >(this IHostApplicationBuilder builder, Action<AppBuilder> appBuilderAction)
+        where TApp : Application
+    {
+        builder.Services.AddAvaloniaHosting<TApp>((_, appBuilder) => appBuilderAction(appBuilder));
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds Avalonia main window to the host's service collection,
+    /// and a <see cref="AppBuilder"/> to create the Avalonia application.
+    /// </summary>
+    /// <param name="builder">The host application builder.</param>
+    /// <param name="appBuilder">The application builder, also used by the previewer.</param>
+    /// <returns>The updated host application builder.</returns>
+    public static IHostBuilder ConfigureAvaloniaHosting<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TApp
+    >(this IHostBuilder builder, Action<IServiceProvider, AppBuilder> appBuilder)
+        where TApp : Application
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddAvaloniaHosting<TApp>(appBuilder);
+        });
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds Avalonia main window to the host's service collection,
+    /// and a <see cref="AppBuilder"/> to create the Avalonia application.
+    /// </summary>
+    /// <param name="builder">The host application builder.</param>
+    /// <param name="appBuilderAction">The application builder, also used by the previewer.</param>
+    /// <returns>The updated host application builder.</returns>
+    public static IHostBuilder ConfigureAvaloniaHosting<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TApp
+    >(this IHostBuilder builder, Action<AppBuilder> appBuilderAction)
+        where TApp : Application
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.AddAvaloniaHosting<TApp>((_, appBuilder) => appBuilderAction(appBuilder));
+        });
+        return builder;
+    }
+}
---
+++ src/Trailblaze.Core/GameServer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Frozen;
+using Ardalis.SmartEnum;
+
+namespace Trailblaze.Core;
+
+public sealed class GameServer : SmartEnum<GameServer, string>
+{
+    public static readonly GameServer None = new(nameof(None), string.Empty);
+    public static readonly GameServer Global = new(nameof(Global), "global");
+    public static readonly GameServer China = new(nameof(China), "cn");
+    public static readonly GameServer Bilibili = new(nameof(Bilibili), "bilibili");
+
+    #region Special
+
+    public static readonly GameServer Overseas = new(nameof(Overseas), "os");
+    public static readonly GameServer Japan = new(nameof(Japan), "jp");
+    public static readonly GameServer Korea = new(nameof(Korea), "kr");
+    public static readonly GameServer Asia = new(nameof(Asia), "asia");
+
+    #endregion
+
+    public static readonly FrozenSet<GameServer> DefaultServers = [Global, China, Bilibili];
+
+    private GameServer(string name, string value)
+        : base(name, value) { }
+}
